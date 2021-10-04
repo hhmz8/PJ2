ab8e27e05ae535ad7d59e95b3499f437c6122d03
sleepySva <hhmz8@mail.umsl.edu>
2021-10-04 06:55:23 -0500

    Testsim basics

--- testsim.c
+++ testsim.c
@@ -11,19 +11,28 @@ testsim.c
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
+#include <time.h>
 #include <ctype.h> //isprint
 #include <unistd.h> //sleep
 
 int main(int argc, char** argv){
 	
+	if (argc != 3) {
+		perror("Error: Testsim requires 2 arguments.");
+		return -1;
+	}
+	pid_t pid = getpid();
+	int sleepTime = atoi(argv[1]);
+	int repeat = atoi(argv[2]);
+	
+	int i;
+	for (i = 0; i < repeat; i++){
+		sleep(sleepTime);
+		char timeBuffer[40];
+		time_t tempTime = time(NULL);
+		strftime(timeBuffer, 40, "%H:%M:%S", localtime(&tempTime));
+		printf("%s %d %d of %d iterations\n", timeBuffer, pid, (i+1), repeat);
+	}
 	printf("End of testsim.\n");
 	return 0;
-}
-
-void parent(){
-	
-}
-
-void child(){
-
 }
