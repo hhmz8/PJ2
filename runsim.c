09e9d710d550dd2cb5e7c7ee39832473f5d37e42
sleepySva <hhmz8@mail.umsl.edu>
2021-10-04 09:50:08 -0500

    Added basic shm read/write, suprisingly works

--- runsim.c
+++ runsim.c
@@ -18,13 +18,19 @@ runsim.c
 // Reference: https://www.tutorialspoint.com/inter_process_communication/inter_process_communication_shared_memory.htm
 #define BUF_SIZE 1024
 #define SHM_KEY 806040
+#define MAX_PRO 20
 
 extern int errno;
+
+struct shmseg {
+   int nlicenses;
+   char buf[BUF_SIZE];
+};
 
 int main(int argc, char** argv) {
 	
 	int option = 0;
-	int nlicenses = 0;
+	int licenseInput = 0;
 
 	// Reference: https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
 	while ((option = getopt(argc, argv, "ht:")) != -1) {
@@ -45,27 +51,10 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 	if (optind < argc) {
-		nlicenses = atoi(argv[optind]);
+		licenseInput = atoi(argv[optind]);
 	}
 	else {
-		nlicenses = 1;
-	}
-	
-	// Shared memory
-	int shmid = shmget(SHM_KEY, BUF_SIZE, 0666|IPC_CREAT);
-	if (shmid == -1) {
-		perror("Error: shmget");
-		return -1;
-	}
-	
-	char* str = (char*) shmat(shmid, 0, 0);
-	if (shmdt(str) == -1) {
-		perror("Error: shmdt");
-		return -1;
-	}
-	if (shmctl(shmid, IPC_RMID, 0) == -1) {
-		perror("Error: shmctl");
-		return -1;
+		licenseInput = 1;
 	}
 	
 	// Fork
@@ -94,6 +83,33 @@ void sigint(int sig){
 }
 
 void parent(){
+	// Shared memory
+	struct shmseg *shmp;
+	int shmid = shmget(SHM_KEY, BUF_SIZE, 0666|IPC_CREAT);
+	if (shmid == -1) {
+		perror("Error: shmget");
+		return -1;
+	}
+	
+	shmp = shmat(shmid, 0, 0);
+	if (shmp == (void *) -1){
+		perror("Error: shmat");
+		return -1;
+	}
+	
+	shmp->nlicenses = 1;
+	
+	/* Deallocation
+	if (shmdt(str) == -1) {
+		perror("Error: shmdt");
+		return -1;
+	}
+	if (shmctl(shmid, IPC_RMID, 0) == -1) {
+		perror("Error: shmctl");
+		return -1;
+	}
+	*/
+	
 	printf("End of parent.\n");
 }
 
@@ -102,13 +118,22 @@ void parent(){
 void child(){
 	signal(SIGINT, sigint);
 	sleep ( 2 );
+	
+	// Shared memory
+	struct shmseg *shmp;
     int shmid = shmget(SHM_KEY, BUF_SIZE, 0666|IPC_CREAT);
 	if (shmid == -1) {
 		perror("Error: shmget");
 		exit(-1);
 	}
 
-	char* str = (char*) shmat(shmid, 0, 0);
+	shmp = shmat(shmid, 0, 0);
+	if (shmp == (void *) -1){
+		perror("Error: shmat");
+		return -1;
+	}
+	
+	printf("nlicenses: %d\n", shmp->nlicenses);
 
 	printf("End of child.\n");
 }
